cb0bde33bc0ab7013c8f12fca74c34443f24811a
kostikf12 <k.yakimtsev@astoundcommerce.com>
2020-02-19 18:29:16 +0200

    1-module and 1-task done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @returns {number}
  */
 function factorial(n) {
+    let result = n;
 
+    if (n == 0) return 1;
+
+    for (let i = n; i > 1; i--) {
+        result = result * (i - 1);
+    }
+
+    return result;
 }
