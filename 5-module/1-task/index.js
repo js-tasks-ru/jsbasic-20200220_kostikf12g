cc4ddd5cb59f8fa0b4216040a8194cf74f563924
kostikf12 <k.yakimtsev@astoundcommerce.com>
2020-03-18 23:45:30 +0200

    5-module and 1-task done

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,30 @@
  * @param {Element} table
  */
 function highlight(table) {
+    for (let trElement of table.rows) {
+        let countTdElements = trElement.cells.length;
+        let statusTdElement = trElement.cells[countTdElements - 1];
+        let genderTdElement = trElement.cells[countTdElements - 2];
+        let ageTdElement = trElement.cells[countTdElements - 3];
+
+        if (statusTdElement.innerText !== "Status") {
+            if (statusTdElement.dataset.available === undefined) { 
+                trElement.setAttribute('hidden', '');
+            } else if (statusTdElement.dataset.available === 'true') {
+                trElement.classList.add('available');
+            } else {
+                trElement.classList.add('unavailable');
+            }
+
+            if (genderTdElement.innerText === 'm') {
+                trElement.classList.add('male');
+            } else {
+                trElement.classList.add('female');
+            }
+
+            if (parseInt(ageTdElement.innerText) < 18) {
+                trElement.style.textDecoration = 'line-through';
+            }
+        }
+    }
 }
