8d1c727d35d7f7c621e6b23e4b722fe12812be95
kostikf12 <k.yakimtsev@astoundcommerce.com>
2020-04-05 21:32:16 +0300

    7-module and 1-task done

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -5,5 +5,12 @@
  */
 
 function promiseClick(button) {
+    let promise = new Promise(function(resolve, reject) {
+        button.addEventListener('click', (event) => {
+            resolve(event);
+        }, { once: true });
+    });
+    
 
+    return promise;
 }
